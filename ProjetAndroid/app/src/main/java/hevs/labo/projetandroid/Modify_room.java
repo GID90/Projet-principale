600e5801906505d50a6286b233d65b775ca703ea
anthonycheseaux <anthony.cheseaux@students.hevs.ch>
2015-10-28 15:46:53 +0100

    modify_room

--- ProjetAndroid/app/src/main/java/hevs/labo/projetandroid/Modify_room.java
+++ ProjetAndroid/app/src/main/java/hevs/labo/projetandroid/Modify_room.java
@@ -1,7 +1,10 @@
 package hevs.labo.projetandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Modify_room extends AppCompatActivity {
 
@@ -9,5 +12,44 @@ public class Modify_room extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_room);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_modify_room, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()){
+
+            case R.id.accueil_menu:
+                Intent intenthome = new Intent(this, MainActivity.class);
+                startActivity(intenthome);
+                return true;
+
+            case R.id.artist_menu:
+                Intent intentartist = new Intent(this, list_artist.class);
+                startActivity(intentartist);
+                return true;
+
+            case R.id.list_artwork_menu:
+                Intent intentartwork = new Intent(this, list_artwork.class);
+                startActivity(intentartwork);
+                return true;
+
+            case R.id.exposition_menu:
+                return true;
+
+            case R.id.parametres_menu:
+                return true;
+        }
+
+        return (super.onOptionsItemSelected(item));
     }
 }
