610488d3ec732f90a7952490ee1c122b2bb2226c
anthonycheseaux <anthony.cheseaux@students.hevs.ch>
2015-10-28 11:47:27 +0100

    card_room

---
+++ ProjetAndroid/app/src/main/java/hevs/labo/projetandroid/Card_room.java
@@ -0,0 +1,55 @@
+package hevs.labo.projetandroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Card_room extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_room);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_card_artwork, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()){
+
+            case R.id.accueil_menu:
+                Intent intenthome = new Intent(this, MainActivity.class);
+                startActivity(intenthome);
+                return true;
+
+            case R.id.artist_menu:
+                Intent intentartist = new Intent(this, list_artist.class);
+                startActivity(intentartist);
+                return true;
+
+            case R.id.list_artwork_menu:
+                Intent intentartwork = new Intent(this, list_artwork.class);
+                startActivity(intentartwork);
+                return true;
+
+            case R.id.exposition_menu:
+                return true;
+
+            case R.id.parametres_menu:
+                return true;
+        }
+
+        return (super.onOptionsItemSelected(item));
+    }
+}
---
+++ ProjetAndroid/app/src/main/java/hevs/labo/projetandroid/Modify_room.java
@@ -0,0 +1,13 @@
+package hevs.labo.projetandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Modify_room extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_room);
+    }
+}
